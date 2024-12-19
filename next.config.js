0d8dfc46079e79f2a42802e6d28419d9f3c64f50
Edwin Olivares <edwin@Edwins-MacBook-Pro.local>
2024-12-18 23:47:16 -0800

    update so that a dist file is produced

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  distDir: "dist", // Specify the output directory for the build
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
